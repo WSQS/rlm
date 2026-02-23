38d262b53eea468165906d7e3a7b053515c23499
Sophomore <17792626+WSQS@users.noreply.github.com>
2026-02-23 20:19:55 +0800

    fix: add class ReplInstance

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
+from code import InteractiveConsole
+import contextlib
+from dataclasses import dataclass
+import io
+import traceback
+
+class ReplInstance:
+    def __init__(self):
+        self.ic = InteractiveConsole()
+    
+    @dataclass
+    class RunResult:
+        out : str
+        err : str
+
+    def run(self,code:str):
+        out = io.StringIO()
+        err = io.StringIO()
+        with contextlib.redirect_stdout(out), contextlib.redirect_stderr(err):
+            try:
+                self.ic.push(code)  # True means more input required
+            except SystemExit:
+                print("SystemExit: exit()/quit() is not allowed in this REPL session.")
+            except Exception:
+                print(traceback.format_exc())
+        return self.RunResult(out.getvalue(),err.getvalue())
+
+
+
 def main():
     print("Hello from rlm!")
+    ic = ReplInstance()
+    r = ic.run("""print("hello,world")""")
+    print(r)
 
 
 if __name__ == "__main__":
